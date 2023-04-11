f857dd859ef72ce46d5cacf591165fe92751c32c
Caleb Wise <calebdwise@gmail.com>
2023-04-11 15:32:49 -0400

    Map is working and page is styled

--- Plan/index.js
+++ Plan/index.js
@@ -1,27 +1,51 @@
-// Initialize and add the map
+//Initialize and add the map
 let map;
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  // The location of Wedding (get from user input on prompt of form)
+  const position = { lat: 35.208282942575416, lng: -84.65539844765875 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
+  // The map, centered at Venue
   map = new Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 15,
     center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: position,
-    title: "Uluru",
+    mapId: "MAP_ID",
   });
 }
 
 initMap();
+
+document
+  .getElementById("addressForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get address from input field
+    var address = document.getElementById("addressInput").value;
+
+    // Use Google Maps geocoding service
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: address }, function (results, status) {
+      if (status === "OK") {
+        var lat = results[0].geometry.location.lat();
+        var lng = results[0].geometry.location.lng();
+
+        // Update map with marker
+        var map = new google.maps.Map(document.getElementById("map"), {
+          center: { lat: lat, lng: lng },
+          zoom: 14,
+          mapId: "Map_ID",
+        });
+        var marker = new google.maps.Marker({
+          map: map,
+          position: { lat: lat, lng: lng },
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  });
