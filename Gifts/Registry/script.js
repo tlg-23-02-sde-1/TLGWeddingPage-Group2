034de9133350d0377ed5f9c652b95fc1eac0c8f4
Christopher Baker <christopher.baker@tlgcohort.com>
2023-04-11 11:46:03 -0700

    commit gifts and registry modifications

---
+++ Gifts/Registry/script.js
@@ -0,0 +1,18 @@
+home.addEventListener("click",()=> {
+    location.href = "/TLGWeddingPage-Group2/index.html"
+})
+plan.addEventListener("click",()=> {
+    location.href = "/TLGWeddingPage-Group2/Plan/plan.html"
+})
+rsvp.addEventListener("click",()=> {
+    location.href = "/TLGWeddingPage-Group2/RSVP/rsvp.html"
+})
+gifts.addEventListener("click",()=> {
+    location.href = "/TLGWeddingPage-Group2/Gifts/gifts.html"
+})
+photos.addEventListener("click",()=> {
+    location.href = "/TLGWeddingPage-Group2/Photos/photos.html"
+})
+login.addEventListener("click",()=> {
+    location.href = "/TLGWeddingPage-Group2/Login/login.html"
+})
