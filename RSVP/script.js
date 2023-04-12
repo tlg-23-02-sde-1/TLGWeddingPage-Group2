61bfba712745021116c623ac991bfe2ff19d1d1a
Justin Chung <justin.chung@tlgcohort.com>
2023-04-12 09:05:28 -0500

    styled photos and RSVP page

---
+++ RSVP/script.js
@@ -0,0 +1,15 @@
+home.addEventListener("click",()=> {
+    location.href = "../index.html"
+})
+plan.addEventListener("click",()=> {
+    location.href = "../Plan/plan.html"
+})
+rsvp.addEventListener("click",()=> {
+    location.href = "./rsvp.html"
+})
+gifts.addEventListener("click",()=>{
+    location.href = "../GIFTS/gifts.html"
+})
+photos.addEventListener("click",()=> {
+    location.href = "../Photos/photos.html"
+})
