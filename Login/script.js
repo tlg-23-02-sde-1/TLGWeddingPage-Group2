6155c80da18960d353f9a241fb9770d98c8d99f4
Justin Chung <justin.chung@tlgcohort.com>
2023-04-12 15:43:13 -0500

    login works, still working on web settings

--- Login/script.js
+++ Login/script.js
@@ -1,3 +1,28 @@
 register.addEventListener("click",()=>{
     location.href="./register.html";
 })
+
+login.addEventListener("submit",(event)=> {
+    event.preventDefault();
+    let username = login.username.value;
+    let password = login.password.value;
+    fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username,
+            password
+        }),
+    })
+    .then((res)=>res.json())
+    .then((data)=>{
+        if(data.success) {
+            location.href=data.url;
+        }
+        else {
+
+        }
+    })
+})
