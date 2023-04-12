1e6bd338ebb1562f0843b4931f126a3b469fc322
Caleb Wise <calebdwise@gmail.com>
2023-04-12 09:42:58 -0400

    added photos of gallery

--- Plan/index.js
+++ Plan/index.js
@@ -49,3 +49,21 @@ document
       }
     });
   });
+home.addEventListener("click", () => {
+  location.href = "../index.html";
+});
+plan.addEventListener("click", () => {
+  location.href = "../Plan/plan.html";
+});
+rsvp.addEventListener("click", () => {
+  location.href = "../RSVP/rsvp.html";
+});
+gifts.addEventListener("click", () => {
+  location.href = "../GIFTS/gifts.html";
+});
+photos.addEventListener("click", () => {
+  location.href = "../Photos/photos.html";
+});
+login.addEventListener("click", () => {
+  location.href = "../Login/login.html";
+});
