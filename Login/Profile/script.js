10aa431f5c0f3b7752a6e0cbb032250d0010dfe9
Justin Chung <justin.chung@tlgcohort.com>
2023-04-13 23:28:05 -0500

    final commit

--- Login/Profile/script.js
+++ Login/Profile/script.js
@@ -13,6 +13,7 @@ gifts.addEventListener("click",()=> {
 photos.addEventListener("click",()=> {
     location.href = "../../Photos/photos.html"
 })
+
 function displayEditPage(url) {
     edit_section.innerHTML = '';
     let ipage = document.createElement("iframe");
@@ -34,25 +35,47 @@ function displaySettings() {
 
     mySwitch.setAttribute("class","switch");
     mySwitch.setAttribute("id","add_timer");
+    
 
     myInput.setAttribute("type","checkbox");
+    myInput.setAttribute("aria-checked","false");
 
     mySpan.setAttribute("class","slider round");
     mySwitch.appendChild(myInput);
     mySwitch.appendChild(mySpan);
     options.appendChild(label);
     options.appendChild(mySwitch);
+
+    let mySwitch2 = document.createElement("label");
+    let myInput2 = document.createElement("input");
+    let mySpan2 = document.createElement("span");
+    let label2 = document.createElement("label");
+
+    label2.innerText = "COLOR";
+
+    mySwitch2.setAttribute("class","switch");
+    mySwitch2.setAttribute("id","chng_color");
+    
+
+    myInput2.setAttribute("type","checkbox");
+    myInput2.setAttribute("aria-checked","false");
+
+    mySpan2.setAttribute("class","slider round");
+    mySwitch2.appendChild(myInput2);
+    mySwitch2.appendChild(mySpan2);
+    options.appendChild(label2);
+    options.appendChild(mySwitch2);
 }
 
-function addTimer() {
+function addTimer(frameDom) {
 
     /*
     Countdown Timer till 15 APR, 2024 10:00AM
     */
-    
+
     let timer_ctner = document.createElement("box");
     timer_ctner.setAttribute("id","timer");
-    
+    frameDom.getElementById("first_section").appendChild(timer_ctner);
     var countDownDate = new Date("Apr 15, 2024 10:00:00").getTime();
 
     var x = setInterval(()=> {
@@ -64,21 +87,86 @@ function addTimer() {
         var minutes = Math.floor((distance%(60*60))/(60));
         var seconds = Math.floor((distance%(60)));
 
-        document.getElementById("timer").textContent = 
+        if(frameDom.getElementById("timer") == null) {
+            return;
+        }
+        frameDom.getElementById("timer").textContent = 
         `${days} days ${hours} hrs ${minutes} mins ${seconds} secs`
 
         if(distance < 0) {
             clearInterval(x);
-            document.getElementById("timer").textContent = "Married!";
+            frameDom.getElementById("timer").textContent = "Married!";
         }
     }, 1000);
+}
+
+function ariaChecker(event) {
+    let checker = event.target.getAttribute("aria-checked");
+    if(checker === "false") {
+        event.target.setAttribute("aria-checked","true");
+        return true;
+    }
+    else {
+        event.target.setAttribute("aria-checked","false");
+        return false;
+    }
+}
+
+function colorChange(frameDom,color) {
+    frameDom.getElementById("first_section").style.color = color;
+    frameDom.getElementById("home").style.color = color;
+    frameDom.getElementById("home").style.borderColor = color;
+    frameDom.getElementById("plan").style.color = color;
+    frameDom.getElementById("plan").style.borderColor = color;
+    frameDom.getElementById("rsvp").style.color = color;
+    frameDom.getElementById("rsvp").style.borderColor = color;
+    frameDom.getElementById("photos").style.color = color;
+    frameDom.getElementById("photos").style.borderColor = color;
+    frameDom.getElementById("gifts").style.color = color;
+    frameDom.getElementById("gifts").style.borderColor = color;
 }
 
 
 buttons_ctner.addEventListener("click", (event)=> {
     if(event.target.textContent === "Edit Landing Page"){
         displayEditPage("../../index.html");
-        color_switch.addEventListener("change", )
+
+        document.getElementById("add_timer").addEventListener("change", (event) => {
+            event.preventDefault();
+            let frameDOM = document.querySelector("iframe").contentDocument;
+            if(ariaChecker(event) === true){
+                let timer = {
+                    timer: true
+                }
+                localStorage.setItem("timer",JSON.stringify(timer));
+                addTimer(frameDOM);
+            }
+            else {
+                let timer = {
+                    timer: false
+                }
+                localStorage.setItem("timer",JSON.stringify(timer));
+                frameDOM.getElementById("timer").remove();
+            }
+        })
+        document.getElementById("chng_color").addEventListener("change",(event)=> {
+            event.preventDefault();
+            let frameDOM = document.querySelector("iframe").contentDocument;
+            if(ariaChecker(event) === true){
+                let color = {
+                    color: true
+                }
+                localStorage.setItem("color",JSON.stringify(color));
+                colorChange(frameDOM,"rgb(0,0,0)");
+            }
+            else {
+                let color = {
+                    color: false
+                }
+                localStorage.setItem("color",JSON.stringify(color));
+                colorChange(frameDOM,"rgb(159,137,83)");
+            }
+        })
     }
     if(event.target.textContent === "Edit Plan Page"){
         displayEditPage("../../Plan/plan.html");
--- RSVP/script.js
+++ RSVP/script.js
@@ -13,3 +13,45 @@ gifts.addEventListener("click",()=>{
 photos.addEventListener("click",()=> {
     location.href = "../Photos/photos.html"
 })
+
+guests.addEventListener("submit",(event)=> {
+    event.preventDefault();
+    let fname = guests.fname.value;
+    let lname = guests.lname.value;
+    let email = guests.email.value;
+    let address = guests.address.value;
+    let city = guests.city.value;
+    let state = guests.state.value;
+    let headCount = guests.headCount.value;
+    let zip = guests.zip.value;
+    let meal = guests.meal.value;
+    let yes_no = guests.yes_no.value;
+
+    fetch("http://localhost:3000/guests", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            fname,
+            lname,
+            email,
+            address,
+            city,
+            state,
+            headCount,
+            zip,
+            meal,
+            yes_no
+        }),
+    })
+    .then((res)=>res.json())
+    .then((data)=>{
+        if(data.success) {
+            location.href=data.url;
+        }
+        else {
+
+        }
+    })
+})
